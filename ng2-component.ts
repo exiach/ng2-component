0cdde347b66cd1ce4ede436749d7eab12ccd0afa
Daniel Canqui <daniel.canqui@jalasoft.com>
2016-07-04 14:14:18 -0400

    First simgle library for angular 2

---
+++ ng2-component.ts
@@ -0,0 +1,8 @@
+import { CardTitleComponent } from './src/card-title/card-title.component';
+
+export * from './src/card-title/card-title.component';
+
+// for angular-cli
+export default {
+    directives: [CardTitleComponent]
+}
---
+++ src/card-title/card-title.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'card-title',
+  templateUrl: 'card-title.component.html',
+  styleUrls: ['card-title.component.css']
+})
+export class CardTitleComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
